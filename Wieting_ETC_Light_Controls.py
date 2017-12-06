901cf4d0344db687c81eb744c3b61614b391240b
Mark McFate <summitt.dweller@gmail.com>
2017-12-05 21:56:03 -0600

    First serial changes made

--- Wieting_ETC_Light_Controls.py
+++ Wieting_ETC_Light_Controls.py
@@ -16,22 +16,19 @@ for serial port control code examples.
 """
 
 import os.path
-import glob
-import argparse
-import tkFileDialog
-import subprocess
-import spur  # use 'pip install spur' to install
+# import tkFileDialog
+# import subprocess
+# import spur  # use 'pip install spur' to install
 import webbrowser
-import json
+# import json
+import serial   # use 'pip install pyserial' to install
 from Tkinter import *  # use 'brew install homebrew/dupes/tcl-tk' to install
-import fileinput
-import StringIO
-from shutil import copyfile
 
-
-def listdir_nohidden(path):
-  return glob.glob(os.path.join(path, '*'))
-
+# import fileinput
+# import StringIO
+# import glob
+# import argparse
+# from shutil import copyfile
 
 def gui():
   """make the GUI version of this app"""
@@ -44,126 +41,70 @@ def gui():
     statusText.set("The help file, 'Wieting_ETC_Light_Controls.md.html' should now be visible in a new browser tab.")
     message.configure(fg="dark green")
   
-  def button_solr_post_callback():
-    """ what to do when the "Post //fileserver/files/? Files to Solr" button is pressed """
+  def button_send_serial_callback():
+    """ what to do when the "Send Serial Test String" button is pressed """
     
     target = entry.get()
-    statusText.set("/opt/solr/bin/post for {} is running on fileserver...".format(target))
+    statusText.set("button_send_serial_callback is sending '{}'...".format(target))
     message.configure(fg="red")
     message.update()
-    
-    fileserver = "192.168.1.24"
-    
-    shell = spur.SshShell(hostname="fileserver", username="mark", missing_host_key=spur.ssh.MissingHostKey.warn)
-    target_path = "/files/{}".format(target)
-    result = shell.run(["/opt/solr/bin/post", "-c", "fs-core", target_path])
-    # result = shell.run(["ls", "-al", "/."])
-    
-    statusText.set("/opt/solr/bin/post on fileserver is complete with a return code of {}".format(result.return_code))
+
+    # Open port at '9600, 8, N, 1', no timeout
+
+    ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+    print(ser.name)  # check which port was really used
+    ser.write(b'hello')  # write a string
+    ser.close()  # close port
+
+    # Open named port at '19200, 8, N, 1', 1s timeout
+
+    with serial.Serial('/dev/ttyS1', 19200, timeout=1) as ser:
+      x = ser.read()  # read one byte
+      s = ser.read(10)  # read up to ten bytes (timeout)
+      line = ser.readline()  # read a '\n' terminated line
+
+    # Open port at '38400, 8, E, 1', non-blocking HW handshaking
+
+    ser = serial.Serial('COM3', 38400, timeout=0,
+    parity = serial.PARITY_EVEN, rtscts = 1)
+    s = ser.read(100)  # read up to one hundred bytes or as much is in the buffer
+
+    # Configuring ports later
+
+    # Get a Serial instance and configure / open it later
+
+    ser = serial.Serial()
+    ser.baudrate = 19200
+    ser.port = 'COM1'
+    ser  # Serial < id = 0xa81c10, open = False > (port='COM1', baudrate=19200, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)"""
+    ser.open()
+    ser.is_open  # True
+    ser.close()
+    ser.is_open  # False
+
+    # Also supported with context manager
+
+    with serial.Serial() as ser:
+      ser.baudrate = 19200
+      ser.port = 'COM1'
+      ser.open()
+      ser.write(b'hello')
+
+    statusText.set("button_send_serial_callback is complete with ser of '{}'...".format(ser))
     message.configure(fg="dark green")
     
-    # print result.output
-  
-  def button_solr_query_callback():
-    """ what to do when the "Query Solr on Fileserver" button is pressed """
-    
-    target = entry.get()
-    statusText.set("Solr query {} is running on fileserver...".format(target))
-    message.configure(fg="red")
-    message.update()
-    
-    shell = spur.SshShell(hostname="fileserver", username="mark", missing_host_key=spur.ssh.MissingHostKey.warn)
-    query = "http://localhost:8983/solr/fs-core/select?wt=json&fl=id&q={}".format(target)
-    result = shell.run(["curl", query])
-    # result = shell.run(["ls", "-al", "/."])
-    
-    d = json.loads(result.output)
-    r = d.get('response')
-    docs = r.get('docs')
-    numFound = r.get('numFound')
-    msg = "Solr query is complete with {} documents found.".format(numFound)
-    if numFound > 0:
-      msg = msg + "  They include:\n\n"
-      for rd in docs:
-        msg = msg + rd.get('id') + "\n"
-    
-    statusText.set(msg)
-    message.configure(fg="dark green")
-  
-  def button_backup_email_callback():
-    """ what to do when the "Backup _Archived_EMail_" button is pressed """
-    
-    """ check if _Archived_EMail_ exists and has files, and if /Volumes/files is mounted """
-    directory = "/Users/mark/Documents/_Archived_EMail_"
-    volume = "/Volumes/files"
-    
-    if not os.path.isdir(directory):
-      statusText.set("Error: The {} directory does not exist!".format(directory))
-      message.configure(fg="red")
-    elif listdir_nohidden(directory) == []:
-      statusText.set("Warning: The {} exists but it is empty!  Nothing to do here.".format(directory))
-      message.configure(fg="red")
-    elif not os.path.isdir(volume):
-      statusText.set("Error: The {} volume is not mounted!".format(volume))
-      message.configure(fg="red")
-    else:
-      statusText.set("home_backup.py is running...")
-      message.configure(fg="red")
-      message.update()
-      
-      bashCommand = "/usr/bin/python /Users/mark/Projects/Python/home_backup/home_backup/home_backup.py /Users/mark/Documents/_Archived_EMail_/ /Volumes/files/STORAGE/_MAIL/ -c ./rsync_config.properties -m mark@tamatoledo.net -l ./home_backup.log --remove -u -d"
-      process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-      output, error = process.communicate()
-      
-      statusText.set(
-        "home_backup.py is finished. You should take steps to clean out {} and the 'For Archival' mailbox.".format(
-          directory))
-      message.configure(fg="dark green")
-  
-  def button_backup_NEAT_Exports_callback():
-    """ what to do when the "Backup NEAT Exports" button is pressed """
-    
-    """ check if iMac500GB exists and has files, and if /Volumes/files is mounted """
-    directory = "/Volumes/iMac500GB"
-    volume = "/Volumes/files"
-    
-    if not os.path.isdir(directory):
-      statusText.set("Error: The {} directory does not exist!".format(directory))
-      message.configure(fg="red")
-    elif listdir_nohidden(directory) == []:
-      statusText.set("Warning: The {} exists but it is empty!  Nothing to do here.".format(directory))
-      message.configure(fg="red")
-    elif not os.path.isdir(volume):
-      statusText.set("Error: The {} volume is not mounted!".format(volume))
-      message.configure(fg="red")
-    else:
-      statusText.set("home_backup.py is running...")
-      message.configure(fg="red")
-      message.update()
-      
-      bashCommand = "/usr/bin/python " \
-                    "/Users/markmcfate/Projects/Python/home_backup/home_backup/home_backup.py " \
-                    "/Volumes/iMac500GB/*.pdf /Volumes/files/NEAT/NEAT_Exports -c " \
-                    "./rsync_config.properties -m " \
-                    "mark@tamatoledo.net -l ./home_backup.log --remove -u -d"
-      process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-      output, error = process.communicate()
-      
-      statusText.set(
-        "home_backup.py to NEAT is finished. You should take steps to clean out {} if necessary.".format(directory))
-      message.configure(fg="dark green")
     
   
-  def button_browse_callback():
-    """ What to do when the Browse button is pressed """
-    filename = tkFileDialog.askopenfilename()
-    entry.delete(0, END)
-    entry.insert(0, filename)
+# def button_browse_callback():
+#   """ What to do when the Browse button is pressed """
+#   filename = tkFileDialog.askopenfilename()
+#   entry.delete(0, END)
+#   entry.insert(0, filename)
   
   # ------------------------------------------------
   
   root = Tk()
-  root.title("McFate Household CPanel v1.0")
+  root.title("Wieting ETC Light Controls v1.0")
   root.geometry("1000x350")
   frame = Frame(root)
   frame.pack()
@@ -178,21 +119,12 @@ def gui():
   separator = Frame(root, height=2, bd=1, relief=SUNKEN)
   separator.pack(fill=X, padx=10, pady=5)
   
-  button_browse = Button(root, text="Browse", command=button_browse_callback)
-  button_backup_email = Button(root, text="Backup _Archived_EMail_ from Mark's MacBook",
-                               command=button_backup_email_callback)
-  button_backup_NEAT_Exports = Button(root, text="Backup NEAT Exports from Mark's iMac",
-                               command=button_backup_NEAT_Exports_callback)
-  button_solr_post = Button(root, text="Post //fileserver/files/? Files to Solr",
-                            command=button_solr_post_callback)
-  button_solr_query = Button(root, text="Query Solr on fileserver", command=button_solr_query_callback)
+# button_browse = Button(root, text="Browse", command=button_browse_callback)
+  button_send_serial = Button(root, text="Send Serial Test String", command=button_send_serial_callback)
   button_help = Button(root, text="Help", command=button_help_callback)
   button_exit = Button(root, text="Exit", command=sys.exit)
-  button_browse.pack()
-  button_backup_email.pack()
-  button_backup_NEAT_Exports.pack()
-  button_solr_post.pack()
-  button_solr_query.pack()
+# button_browse.pack()
+  button_send_serial.pack()
   button_help.pack()
   button_exit.pack()
   
