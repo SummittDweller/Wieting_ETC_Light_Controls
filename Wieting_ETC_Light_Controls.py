ac9038256ee4d1cb98e0868b9f4191b3d2584d8b
SummittDweller <4Score&7>
2017-12-07 21:23:46 -0600

    Much improved

--- Wieting_ETC_Light_Controls.py
+++ Wieting_ETC_Light_Controls.py
@@ -37,48 +37,71 @@ def gui():
   # --- Define the callbacks
 
   def button_help_callback():
-    """ what to do when the "Help" button is pressed """
-    
     filename = "./Wieting_ETC_Light_Controls.md.html"
     webbrowser.open('file://' + os.path.realpath(filename))
     statusText.set("The help file, 'Wieting_ETC_Light_Controls.md.html' should now be visible in a new browser tab.")
     message.configure(fg="dark green")
   
-  def button_set_level_callback():
-    level = entry.get()
-    statusText.set("Selected Fader(s) will be set to {}".format(level))
-    message.configure(fg="blue")
-    message.update()
-  
   def button_select_fader1_callback():
-    set_fader(1)
+    set_fader(1, "Oleos")
 
   def button_select_fader2_callback():
-    set_fader(2)
+    set_fader(2, "House Lights")
 
   def button_select_fader3_callback():
-    set_fader(3)
+    set_fader(3, "Back of House")
 
   def button_select_fader4_callback():
-    set_fader(4)
+    set_fader(4, "Stage/Wing Floods")
 
   def button_select_fader5_callback():
-    set_fader(5)
+    set_fader(5, "Spots 1 and 3")
 
   def button_select_fader6_callback():
-    set_fader(6)
+    set_fader(6, "Spots 2 and 4")
 
-  def set_fader(f):
+  def button_select_all_faders_callback():
+    set_fader(1, " ")
+    set_fader(2, " ")
+    set_fader(3, " ")
+    set_fader(4, " ")
+    set_fader(5, " ")
+    set_fader(6, "ALL")
+
+  def set_fader(f, desc):
     level = entry.get()
-    statusText.set("Setting Fader {0} to level '{1}'".format(f, level))
-    message.configure(fg="blue")
-    message.update()
     code = "SF{0}.{1}".format(f, level) + "\r\n"
-    ser.write(code)                                  # write fader x (SFx.level) string
-    sleep(1)                               # wait one second
+    try:
+      ser.write(code)
+    except:
+      msg = "Serial connection error: {}".format(sys.exc_info()[0])
+      statusText.set(msg)
+      message.configure(fg="red")
+      message.update()
+
+    sleep(1)                      # wait one second
     bytesToRead = ser.inWaiting()
     result = ser.read(bytesToRead)
-    msg = "Sent '" + code + "' and response is: " + result
+    msg = "Set Fader for '{0}' ({1}) to Level '{2}' and the response is: {3}".format(desc, f, level, result)
+    statusText.set(msg)
+    message.configure(fg="dark green")
+    message.update()
+
+  def button_send_raw_callback():
+    raw = entry.get()
+    code = "{0}\r\n".format(raw)
+    try:
+      ser.write(code)
+    except:
+      msg = "Serial connection error: {}".format(sys.exc_info()[0])
+      statusText.set(msg)
+      message.configure(fg="red")
+      message.update()
+
+    sleep(2)  # wait two seconds
+    bytesToRead = ser.inWaiting()
+    result = ser.read(bytesToRead)
+    msg = "Raw command '{0}' sent with response:\r\n{1}".format(raw, result)
     statusText.set(msg)
     message.configure(fg="dark green")
     message.update()
@@ -101,14 +124,14 @@ def gui():
   
   root = Tk()
   root.title("Wieting ETC Light Controls v1.0")
-  root.geometry("1000x500")
+  root.geometry("500x750")
   frame = Frame(root)
   frame.pack()
   
   statusText = StringVar(root)
   statusText.set("Browse to open a file OR enter a Unison AV/Serial 1.0 command...")
   
-  label = Label(root, text="Serial command or selected file/folder:")
+  label = Label(root, text="Enter a serial command (raw), fader level, or browse for a file:")
   label.pack(padx=10)
   entry = Entry(root, width=80, justify='center')
   entry.pack(padx=10)
@@ -116,24 +139,26 @@ def gui():
   separator.pack(fill=X, padx=10, pady=5)
   
   button_browse = Button(root, text="Browse", command=button_browse_callback)
-  button_set_level = Button(root, text="Specify Fader Level", command=button_set_level_callback)
-  button_select_fader1 = Button(root, text="Set Fader 1 to the Specified Level", command=button_select_fader1_callback)
-  button_select_fader2 = Button(root, text="Set Fader 2 to the Specified Level", command=button_select_fader2_callback)
-  button_select_fader3 = Button(root, text="Set Fader 3 to the Specified Level", command=button_select_fader3_callback)
-  button_select_fader4 = Button(root, text="Set Fader 4 to the Specified Level", command=button_select_fader4_callback)
-  button_select_fader5 = Button(root, text="Set Fader 5 to the Specified Level", command=button_select_fader5_callback)
-  button_select_fader6 = Button(root, text="Set Fader 6 to the Specified Level", command=button_select_fader6_callback)
+  button_send_raw = Button(root, text="Send Raw Command", command=button_send_raw_callback)
+  button_select_fader1 = Button(root, text="Set Fader for Oleos (1)", command=button_select_fader1_callback)
+  button_select_fader2 = Button(root, text="Set Fader for House Lights (2)", command=button_select_fader2_callback)
+  button_select_fader3 = Button(root, text="Set Fader for Back of House (3)", command=button_select_fader3_callback)
+  button_select_fader4 = Button(root, text="Set Fader for Stage/Wing Floods (4)", command=button_select_fader4_callback)
+  button_select_fader5 = Button(root, text="Set Fader for Spots 1 & 2 (5)", command=button_select_fader5_callback)
+  button_select_fader6 = Button(root, text="Set Fader for Spots 2 & 4 (6)", command=button_select_fader6_callback)
+  button_select_all_faders = Button(root, text="Set All Faders (1-6)", command=button_select_all_faders_callback)
   button_close_port = Button(root, text="Close the Serial Port", command=button_close_port_callback)
   button_help = Button(root, text="Help", command=button_help_callback)
   button_exit = Button(root, text="Exit", command=sys.exit)
   button_browse.pack()
-  button_set_level.pack()
+  button_send_raw.pack()
   button_select_fader1.pack()
   button_select_fader2.pack()
   button_select_fader3.pack()
   button_select_fader4.pack()
   button_select_fader5.pack()
   button_select_fader6.pack()
+  button_select_all_faders.pack()
   button_close_port.pack()
   button_help.pack()
   button_exit.pack()
